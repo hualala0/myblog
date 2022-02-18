2b24ced971bd52ae2b85decb17ee761342f5a61f
hualala0 <1151723949@qq.com>
2022-02-18 22:25:32 +0800

    fix bug

--- src/components/format.tsx
+++ src/components/format.tsx
@@ -1,38 +1,36 @@
+// import React from 'react'
+// import { MDXProvider } from '@mdx-js/react'
+// import { MDXRenderer } from 'gatsby-plugin-mdx'
+// import { FormatH1, FormatH2, FormatH3, FormatH4, FormatH5, FormatH6, FormatPre } from './MDXFormat'
+// import { Layout } from './Layout'
+// import { FormatStrong } from './MDXFormat/FormatStrong'
+// import { FormatEm } from './MDXFormat/FormatEm'
+// import { FormatLink } from './MDXFormat/FormatLink'
 
+// // const MyH1 = (props: any) => <h1 style={{color: 'tomato'}} {...props} />
+// const format = ({ pageContext: { edge } }: { pageContext: any }) => {
+//   return (
+//     <Layout>
+//       <MDXProvider
+//         components={{
+//           // Map HTML element tag to React component
+//           h1: FormatH1,
+//           h2: FormatH2,
+//           h3: FormatH3,
+//           h4: FormatH4,
+//           h5: FormatH5,
+//           h6: FormatH6,
+//           pre: FormatPre,
+//           strong: FormatStrong,
+//           em: FormatEm,
+//           a: FormatLink,
+//           // Or define component inline
+//         }}
+//       >
+//         <MDXRenderer>{edge.node.body}</MDXRenderer>
+//       </MDXProvider>
+//     </Layout>
+//   )
+// }
 
-import React from 'react'
-import { MDXProvider } from '@mdx-js/react'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { FormatH1, FormatH2, FormatH3, FormatH4, FormatH5, FormatH6, FormatPre } from './MDXFormat'
-import { Layout } from './Layout'
-import { FormatStrong } from './MDXFormat/FormatStrong'
-import { FormatEm } from './MDXFormat/FormatEm'
-import { FormatLink } from './MDXFormat/FormatLink'
-
-// const MyH1 = (props: any) => <h1 style={{color: 'tomato'}} {...props} />
-const format = ({ pageContext: { edge } }: { pageContext: any }) => {
-  return (
-    <Layout>
-      <MDXProvider
-        components={{
-          // Map HTML element tag to React component
-          h1: FormatH1,
-          h2: FormatH2,
-          h3: FormatH3,
-          h4: FormatH4,
-          h5: FormatH5,
-          h6: FormatH6,
-          pre: FormatPre,
-          strong: FormatStrong,
-          em: FormatEm,
-          a: FormatLink,
-          // Or define component inline
-        }}
-      >
-        <MDXRenderer>{edge.node.body}</MDXRenderer>
-      </MDXProvider>
-    </Layout>
-  )
-}
-
-export default format
+// export default format
