6ac918410451ce0572163a404dd512a810bc9cb7
hualala0 <1151723949@qq.com>
2022-02-18 22:21:40 +0800

    fix bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
         // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ['/mono-blue.css'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react'
 import head from '../../static/images/head.jpg'
 
